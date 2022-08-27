93ad2967ea225dbfc4037a9e99db70dbce815713
eugeneoei <eugeneoei89@gmail.com>
2022-08-27 16:28:08 +0800

    on successful initialisation, display 'done initialising' message

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
 describe("App initialisation", () => {
@@ -7,5 +7,19 @@ describe("App initialisation", () => {
         render(<App />);
         const spinner = screen.getByRole("progressbar");
         expect(spinner).toBeInTheDocument();
+    });
+
+    it('should display "done initialising" message after initialisation process completes', async () => {
+        render(<App />);
+        // * https://testing-library.com/docs/dom-testing-library/api-async/#waitfor
+        await waitFor(
+            () => {
+                const message = screen.getByText(/done initialising/i);
+                expect(message).toBeInTheDocument();
+            },
+            {
+                timeout: 3000
+            }
+        );
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-    return (
-        <div className="text-center">
-            <span role="progressbar">
-                <FontAwesomeIcon
-                    icon={faSpinner}
-                    className="animate-spin"
-                />
-            </span>
-        </div>
-    );
-}
+const App = () => {
+    const [isInitialising, seItInitialising] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            seItInitialising(false);
+        }, 500);
+    }, []);
+
+    if (isInitialising) {
+        return (
+            <div className="text-center">
+                <span role="progressbar">
+                    <FontAwesomeIcon
+                        icon={faSpinner}
+                        className="animate-spin"
+                    />
+                </span>
+            </div>
+        );
+    }
+
+    return <p>done initialising</p>;
+};
 
 export default App;
